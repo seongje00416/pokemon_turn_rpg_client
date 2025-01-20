8a4b61fa118bc6bc2e7e688b6cbeb8c77ac92244
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2025-01-20 13:54:46 +0900

    feat: 캐릭터 선택 페이지 제작중

--- src/page/battle/SelectPokemonPage.tsx
+++ src/page/battle/SelectPokemonPage.tsx
@@ -1,10 +1,133 @@
-
+import {
+    PokemonCardImage,
+    MainContainer,
+    MyTeamContainer,
+    OppositeTeamContainer,
+    PokemonSelectCard,
+    PokemonSelectContainer,
+    PokemonCardName,
+    PokemonCardNameText,
+    PokemonSelectWrapper,
+    TeamUserWrapper,
+    TeamEntryWrapper,
+    TeamUserText,
+    TeamEntryPokemonCard, TeamEntryPokemonCardImage, TeamEntryPokemonCardText
+} from "@/style/battle/SelectPokemonPage_Style"
 
 export const SelectPokemonPage = () => {
     return (
-        <div>
-            TEST
-        </div>
+        <MainContainer>
+            <OppositeTeamContainer>
+                <TeamUserWrapper>
+                    <TeamUserText> 적군 정보 </TeamUserText>
+                </TeamUserWrapper>
+                <TeamEntryWrapper>
+                    <TeamEntryPokemonCard>
+                        <TeamEntryPokemonCardImage src="src/assets/pokemon/초염몽_앞.gif" />
+                        <TeamEntryPokemonCardText> 초염몽 Lv.50 </TeamEntryPokemonCardText>
+                    </TeamEntryPokemonCard>
+                    <TeamEntryPokemonCard>
+                        <TeamEntryPokemonCardImage src="src/assets/pokemon/엠페르트_앞.gif" />
+                        <TeamEntryPokemonCardText> 엠페르트 Lv.50 </TeamEntryPokemonCardText>
+                    </TeamEntryPokemonCard>
+                    <TeamEntryPokemonCard>
+                        <TeamEntryPokemonCardImage src="src/assets/pokemon/후딘_앞.gif" />
+                        <TeamEntryPokemonCardText> 후딘 Lv.50 </TeamEntryPokemonCardText>
+                    </TeamEntryPokemonCard>
+                    <TeamEntryPokemonCard>
+                        <TeamEntryPokemonCardImage src="src/assets/pokemon/메가니움_앞.gif" />
+                        <TeamEntryPokemonCardText> 메가니움 Lv.50 </TeamEntryPokemonCardText>
+                    </TeamEntryPokemonCard>
+                </TeamEntryWrapper>
+            </OppositeTeamContainer>
+            <MyTeamContainer>
+                <TeamUserWrapper>
+                    <TeamUserText> 아군 정보 </TeamUserText>
+                </TeamUserWrapper>
+                <TeamEntryWrapper>
+                    <TeamEntryPokemonCard>
+                        <TeamEntryPokemonCardImage src="src/assets/pokemon/라이츄_앞.gif" />
+                        <TeamEntryPokemonCardText> 라이츄 Lv.50 </TeamEntryPokemonCardText>
+                    </TeamEntryPokemonCard>
+                    <TeamEntryPokemonCard>
+                        <TeamEntryPokemonCardImage src="src/assets/pokemon/딱구리_앞.gif" />
+                        <TeamEntryPokemonCardText> 딱구리 Lv.50 </TeamEntryPokemonCardText>
+                    </TeamEntryPokemonCard>
+                    <TeamEntryPokemonCard>
+                        <TeamEntryPokemonCardImage src="src/assets/pokemon/후딘_앞.gif" />
+                        <TeamEntryPokemonCardText> 후딘 Lv.50 </TeamEntryPokemonCardText>
+                    </TeamEntryPokemonCard>
+                    <TeamEntryPokemonCard>
+                        <TeamEntryPokemonCardImage src="src/assets/pokemon/푸크린_앞.gif" />
+                        <TeamEntryPokemonCardText> 푸크린 Lv.50 </TeamEntryPokemonCardText>
+                    </TeamEntryPokemonCard>
+                </TeamEntryWrapper>
+            </MyTeamContainer>
+            <PokemonSelectContainer>
+                <PokemonSelectWrapper>
+                    <PokemonSelectCard>
+                        <PokemonCardImage src="src/assets/pokemon/초염몽_앞.gif" />
+                        <PokemonCardName>
+                            <PokemonCardNameText> 초염몽 </PokemonCardNameText>
+                        </PokemonCardName>
+                    </PokemonSelectCard>
+                    <PokemonSelectCard>
+                        <PokemonCardImage src="src/assets/pokemon/메가니움_앞.gif" />
+                        <PokemonCardName>
+                            <PokemonCardNameText> 메가니움 </PokemonCardNameText>
+                        </PokemonCardName>
+                    </PokemonSelectCard>
+                    <PokemonSelectCard>
+                        <PokemonCardImage src="src/assets/pokemon/엠페르트_앞.gif" />
+                        <PokemonCardName>
+                            <PokemonCardNameText> 엠페르트 </PokemonCardNameText>
+                        </PokemonCardName>
+                    </PokemonSelectCard>
+                    <PokemonSelectCard>
+                        <PokemonCardImage src="src/assets/pokemon/망나뇽_앞.gif" />
+                        <PokemonCardName>
+                            <PokemonCardNameText> 망나뇽 </PokemonCardNameText>
+                        </PokemonCardName>
+                    </PokemonSelectCard>
+                    <PokemonSelectCard>
+                        <PokemonCardImage src="src/assets/pokemon/괴력몬_앞.gif" />
+                        <PokemonCardName>
+                            <PokemonCardNameText> 괴력몬 </PokemonCardNameText>
+                        </PokemonCardName>
+                    </PokemonSelectCard>
+                    <PokemonSelectCard>
+                        <PokemonCardImage src="src/assets/pokemon/라이츄_앞.gif" />
+                        <PokemonCardName>
+                            <PokemonCardNameText> 라이츄 </PokemonCardNameText>
+                        </PokemonCardName>
+                    </PokemonSelectCard>
+                    <PokemonSelectCard>
+                        <PokemonCardImage src="src/assets/pokemon/딱구리_앞.gif" />
+                        <PokemonCardName>
+                            <PokemonCardNameText> 딱구리 </PokemonCardNameText>
+                        </PokemonCardName>
+                    </PokemonSelectCard>
+                    <PokemonSelectCard>
+                        <PokemonCardImage src="src/assets/pokemon/팬텀_앞.gif" />
+                        <PokemonCardName>
+                            <PokemonCardNameText> 팬텀 </PokemonCardNameText>
+                        </PokemonCardName>
+                    </PokemonSelectCard>
+                    <PokemonSelectCard>
+                        <PokemonCardImage src="src/assets/pokemon/푸크린_앞.gif" />
+                        <PokemonCardName>
+                            <PokemonCardNameText> 푸크린 </PokemonCardNameText>
+                        </PokemonCardName>
+                    </PokemonSelectCard>
+                    <PokemonSelectCard>
+                        <PokemonCardImage src="src/assets/pokemon/후딘_앞.gif" />
+                        <PokemonCardName>
+                            <PokemonCardNameText> 후딘 </PokemonCardNameText>
+                        </PokemonCardName>
+                    </PokemonSelectCard>
+                </PokemonSelectWrapper>
+            </PokemonSelectContainer>
+        </MainContainer>
     )
 }
 
---
+++ src/style/battle/SelectPokemonPage_Style.ts
@@ -0,0 +1,124 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    background-color: whitesmoke;
+`
+// 적군 팀 컨테이너
+export const OppositeTeamContainer = styled.div`
+    width: 30%;
+    height: 80%;
+    border: 1px solid black;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+`
+// 아군 팀 컨테이너
+export const MyTeamContainer = styled.div`
+    width: 30%;
+    height: 80%;
+    border: 1px solid black;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+`
+export const TeamUserWrapper = styled.div`
+    display: flex;
+    height: 10%;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 3px dotted gray;
+    margin-bottom: 20px;
+    width: 80%;
+`
+export const TeamUserText = styled.p`
+    font-weight: bold;
+    font-size: 20px;
+`
+export const TeamEntryWrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    width: 100%;
+    height: 80%;
+`
+export const TeamEntryPokemonCard = styled.div`
+    border: 1px solid gray;
+    border-radius: 5px;
+    display: flex;
+    padding-top: 20px;
+    margin: 25px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: skyblue;
+`
+export const TeamEntryPokemonCardImage = styled.img`
+    height: 70%;
+    width: auto;
+`
+export const TeamEntryPokemonCardText = styled.p`
+    text-align: center;
+    font-weight: bold;
+    font-size: 18px;
+`
+// 포켓몬 선택 컨테이너
+export const PokemonSelectContainer = styled.div`
+    width: 20%;
+    height: 80%;
+    background-color: white;
+`
+export const PokemonSelectWrapper = styled.div`
+    display: flex;
+    width: 100%;
+    height: 85%;
+    overflow-y: scroll;
+    border: 1px solid black;
+    border-radius: 10px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+    padding-top: 25%;
+    padding-bottom: 5%;
+`
+export const PokemonSelectCard = styled.div`
+    width: 80%;
+    height: 8%;
+    padding: 10px;
+    border: 1px solid lightgray;
+    border-radius: 10px;
+    align-items: center;
+    background-color: whitesmoke;
+    display: flex;
+    justify-content: center;
+    
+    &:hover {
+        cursor: pointer;
+        opacity: 0.7;
+    }
+`
+export const PokemonCardImage = styled.img`
+    width: auto;
+    height: 100%;
+`
+export const PokemonCardName = styled.div`
+    display: flex;
+    width: 80%;
+    justify-content: center;
+`
+export const PokemonCardNameText = styled.p`
+    font-weight: bold;   
+    font-size: 20px;
+`
